95f373bdfdce83005d55752718461df6ab5b9b72
Nour Chamakh <50649514+nourChamakh1@users.noreply.github.com>
2021-08-02 03:57:26 +0100

    création-d'une-barre-de-menu-intelligente
    création-d'une-barre-de-menu-intelligente

---
+++ menu.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">    <img src="https://getbootstrap.com/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <ul class="navbar-nav ">
+      <li class="nav-item">
+         <a class="nav-link" href="#">Forum</a>
+      </li>
+  </ul>
+    <ul class="navbar-nav ml-md-auto">
+      <li class="nav-item">
+         <a class="nav-link" href="inscription.php"> S'inscrire</a>
+      </li>
+      <li class="nav-item">
+         <a class="nav-link" href="connexion.php"> Se connecter</a>
+      </li>
+    </ul>
+  </div>
+ </nav>
